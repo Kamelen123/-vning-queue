944519500da545b2a95c8833bf271d0a23b8fd43
toro501 <toro501@PC28126>
2023-10-31 12:02:35 +0100

    Add project files.

---
+++ Övning queue/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.Cache;
+
+namespace Övning_queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Person person = new Person()
+            //{
+            //    Age = 20,
+            //    Name = "Test",
+            //};
+            Läkare Anton = new Läkare()
+            {
+
+                Name = "Anton",
+                Age = 25,
+                Salary = 45000,
+                ID = 123415,
+                WorkTime = 45,
+
+            };
+            Sjuksköterska torbjörn = new Sjuksköterska()
+            {
+                Name = "Torbjörn",
+                Age = 28,
+                Salary = 50000,
+                ID = 343253,
+                WorkTime = 40
+            };
+            patient Anas = new patient()
+            {
+                Name = "Anas",
+                Age = 35,
+                TimeOfReg = 10,
+                sjukdom = "Covid",
+                VacQue = 1,
+            };
+            patient Alf = new patient()
+            {
+                Name = "Alf",
+                Age = 30,
+                TimeOfReg = 11,
+                sjukdom = "Covid",
+                VacQue = 2,
+            };
+            torbjörn.BlodTest();
+            Anton.SkrivaRecept();
+
+            //Dictionary<int, patient> dict = new Dictionary<int, Employee>();
+
+            Queue<patient> VacQueue = new Queue<patient>();
+            VacQueue.Enqueue(Anas);
+            VacQueue.Enqueue(Alf);
+
+            patient deleted = VacQueue.Dequeue();
+            Console.WriteLine(deleted.Name + " Is Deleted From Que");
+
+            patient peekPatient = VacQueue.Peek();
+            Console.WriteLine(peekPatient.Name);
+
+        }
+    }
+}
---
+++ Övning queue/person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning_queue
+{
+    internal class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+    }
+     internal class Läkare : Person
+    {
+        public int WorkTime { get; set; }
+        public int Salary { get; set; }
+
+        public int ID { get; set; }
+        public void SkrivaRecept()
+        {
+            Console.WriteLine("Läkaren skriver recept");
+        }
+
+    }
+    internal class Sjuksköterska : Person
+    {
+        public int ID { get; set; }
+        public int WorkTime { get; set; }
+        public int Salary { get; set; }
+        public void BlodTest()
+        {
+            Console.WriteLine("Sjuksköterskan tar blod");
+        }
+    }
+    internal class patient : Person
+    {
+        public string sjukdom { get; set; }
+        public int TimeOfReg { get; set; }
+
+        public int VacQue { get; set; }
+
+    }
+}
